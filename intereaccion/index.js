c50be12d291f934418a45dac8bd75e3de5fb59f0
kiyotaka <balldarwin638@gmail.com>
2021-01-18 01:26:04 -0800

    se finalizo el proyecto

--- intereaccion/index.js
+++ intereaccion/index.js
@@ -1,7 +1,7 @@
 let prueba = document.getElementById("inter");
 let foorer = document.getElementById("footer");
 let desplegar = document.getElementById("accionDesplegar");
-let domDesaparecer = document.querySelector(".layaut-container");
+let domDesaparecer = document.querySelectorAll(".layaut-container__1");
 
 
 
@@ -18,15 +18,61 @@ function accion()
     {
         desplegar.childNodes[5].removeAttribute("id");
         desplegar.childNodes[7].setAttribute("id" ,"remove");
-        domDesaparecer.removeAttribute("id");
+        for(let i in domDesaparecer)
+        {
+            domDesaparecer[i].removeAttribute("id");
+        }
+        
        
     }
     else
     {
         desplegar.childNodes[7].removeAttribute("id");
         desplegar.childNodes[5].setAttribute("id" ,"remove");
-        domDesaparecer.setAttribute("id" ,"remove");
+
+        for(let i in domDesaparecer)
+        {
+            domDesaparecer[i].setAttribute("id" ,"remove");
+        }
+        
     }
    
 
 }
+
+
+
+
+
+// let prueba = document.getElementById("inter");
+// let foorer = document.getElementById("footer");
+// let desplegar = document.getElementById("accionDesplegar");
+// let domDesaparecer = document.querySelector(".layaut-container__1");
+
+
+
+// console.log(domDesaparecer)
+
+
+
+// desplegar.addEventListener("click", accion);
+
+// function accion()
+// {
+
+//     if( desplegar.childNodes[5].getAttribute("id") === "remove")
+//     {
+//         desplegar.childNodes[5].removeAttribute("id");
+//         desplegar.childNodes[7].setAttribute("id" ,"remove");
+//         domDesaparecer.removeAttribute("id");
+       
+//     }
+//     else
+//     {
+//         desplegar.childNodes[7].removeAttribute("id");
+//         desplegar.childNodes[5].setAttribute("id" ,"remove");
+//         domDesaparecer.setAttribute("id" ,"remove");
+//     }
+   
+
+// }
